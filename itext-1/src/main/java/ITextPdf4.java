ae1d57a1c85a0c00fd29720815ea84a032736ddb
zhangzhixi <1820712542@qq.com>
2024-02-10 23:58:52 +0800

    添加导出pdf，从数据库中取出数据

--- itext-1/src/main/java/ITextPdf4.java
+++ itext-1/src/main/java/ITextPdf4.java
@@ -12,10 +12,13 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -27,15 +30,70 @@ public class ITextPdf4 {
     public static final String DEST = "results/chapter01/united_states.pdf";
 
     public static void main(String[] args) throws IOException {
-        new ITextPdf4().createPdf(DEST);
+        //new ITextPdf4().createPdfToCsv(DEST);
+        new ITextPdf4().createPdfToDb(DEST);
     }
 
-    public void createPdf(String dest) throws IOException {
+    /**
+     * 从数据库创建PDF
+     *
+     * @param dest 输出文件路径
+     * @throws IOException IO异常
+     */
+    public void createPdfToDb(String dest) throws IOException {
+        try (PdfWriter writer = new PdfWriter(dest);
+             PdfDocument pdf = new PdfDocument(writer);
+             Document document = new Document(pdf);
+        ) {
+            // 设置文档的页边距
+            document.setMargins(20, 20, 20, 20);
+
+            // 创建只有两列的表格，各占50%宽度
+            Table table = new Table(new float[]{50f, 50f});
+            // 设置表格宽度为页面宽度的100%
+            table.setWidthPercent(100);
+            // 设置表格文字居中
+            table.setTextAlignment(TextAlignment.CENTER);
+
+            // DB
+            List<Student> students = new ArrayList<>();
+            Student student = new Student();
+            student.setName("zhangsan");
+            student.setAge(18);
+            Student student2 = new Student();
+            student2.setName("李四");
+            student2.setAge(20);
+            students.add(student);
+            students.add(student2);
+
+            // 创建字体（中文）
+            PdfFont fontGarbled = PdfFontFactory.createFont("STSongStd-Light", "UniGB-UCS2-H", false);
+
+            // 添加表头
+            table.addHeaderCell(new Cell().add(new Paragraph("姓名").setFont(fontGarbled)).setWidth(50f));
+            table.addHeaderCell(new Cell().add(new Paragraph("年龄").setFont(fontGarbled)).setWidth(50f));
+
+            // 添加学生信息
+            for (Student s : students) {
+                table.addCell(new Cell().add(new Paragraph(s.getName()).setFont(fontGarbled)).setWidth(50f));
+                table.addCell(new Cell().add(new Paragraph(String.valueOf(s.getAge())).setFont(fontGarbled)).setWidth(50f));
+            }
+            document.add(table);
+        }
+    }
+
+    /**
+     * 从CSV文件创建PDF
+     *
+     * @param dest 输出文件路径
+     * @throws IOException IO异常
+     */
+    public void createPdfToCsv(String dest) throws IOException {
         try (PdfWriter writer = new PdfWriter(dest);
              PdfDocument pdf = new PdfDocument(writer);
              Document document = new Document(pdf, PageSize.A4.rotate());
-             BufferedReader br = new BufferedReader(new FileReader(DATA));) {
-
+             BufferedReader br = new BufferedReader(new FileReader(DATA));
+        ) {
             // 设置文档的页边距
             document.setMargins(20, 20, 20, 20);
             // 创建字体：普通字体和加粗字体
@@ -74,5 +132,28 @@ public class ITextPdf4 {
                 table.addCell(new Cell().add(new Paragraph(tokenizer.nextToken()).setFont(font)));
             }
         }
+
+    }
+
+    class Student {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        private int age;
     }
 }
